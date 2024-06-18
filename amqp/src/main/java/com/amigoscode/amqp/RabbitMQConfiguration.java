e02b903d179778bc58536c2f50b6fef5ebaaec7a
fathiamir37 <fathiamir37@gmail.com>
2024-06-18 08:27:34 +0330

    micro

---
+++ amqp/src/main/java/com/amigoscode/amqp/RabbitMQConfiguration.java
@@ -0,0 +1,29 @@
+package com.amigoscode.amqp;
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class RabbitMQConfiguration {
+
+    private final ConnectionFactory connectionFactory;
+
+    @Bean
+    public AmqpTemplate amqpTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConvertor());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter jacksonConvertor() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
