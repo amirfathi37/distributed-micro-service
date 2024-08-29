edbe295debb880c9433dd5dc2a5cf2ccb8b42f9d
fathiamir37 <fathiamir37@gmail.com>
2024-08-29 10:26:38 +0330

    add rabbit listener to notification micro

--- notification/src/main/java/com/amigoscode/notification/rabbitmq/NotificationConsumer.java
+++ notification/src/main/java/com/amigoscode/notification/rabbitmq/NotificationConsumer.java
@@ -1,2 +1,21 @@
-package com.amigoscode.notification.rabbitmq;public class NotificationConsumer {
+package com.amigoscode.notification.rabbitmq;
+
+import com.amigoscode.client.notification.NotificationDTO;
+import com.amigoscode.notification.NotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.notification}")
+    public void getNotification(NotificationDTO notificationDTO) {
+        log.info("received this notification: {}", notificationDTO);
+        notificationService.saveNotificationData(notificationDTO);
+    }
 }
